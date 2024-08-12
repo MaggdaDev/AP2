afa48968634a10dc4ec198c7fa542e120bead00c
MaggdaDev <davidbetko05@gmail.com>
2024-08-12 22:13:54 +0200

    started tra

---
+++ TRA/tra.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib as plt
+from scipy.optimize import curve_fit as fit
+
+## Mess-Aufgabe 1
+
+uBEArbeitspunkt = np.array([
+
+])
+
+uBEArbeitspunktMean = np.mean(uBEArbeitspunkt)
+
+uCEArbeitspunkt = np.array([
+
+])
+
+iCArbeitspunkt = np.array([
+
+])
+
+rCArbeitspunktKiloOhm = np.array([
+    1.0,5.0,10.0
+])
+
+## Mess-Aufgabe 2
+
+uEVerstaerkungMitKondOhneRL = np.array([
+
+])
+
+uAVerstaerkungMitKondOhneRL = np.array([
+
+])
+
+rCVerstaerkungKiloOhmMitKondOhneRL = np.array([
+
+])
+
+uEVerstaerkungOhneKondOhneRL = np.array([
+
+])
+
+uAVerstaerkungOhneKondOhneRL = np.array([
+
+])
+
+rCVerstaerkungKiloOhmOhneKondOhneRL = np.array([
+
+])
+
+uEVerstaerkungMitKondMitRL = np.array([
+
+])
+
+uAVerstaerkungMitKondMitRLL = np.array([
+
+])
+
+rCVerstaerkungKiloOhmMitKondMitRL = np.array([
+
+])
+
+
+## Mess-Aufgabe 3
+rCFrequenzGangKiloOhm = 10.0
+fFrequenzGang = np.array([
+    6,
+    250
+])
+
+uEFrequenzgang = np.array([
+
+])
+
+uAFrequenzGang = np.array([
+
+])
+
+phasenDiffZeitbereichFrequenzGang = np.array([
+
+])
+
+phasenDiffLissajousFrequenzGang = np.array([
+
+])
+
+
+## Mess-Aufgabe 4
+
+
+
+
